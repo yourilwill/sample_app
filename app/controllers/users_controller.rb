7fd7595211c4b60966992abab4c695f6e6cacb6e
yourilwill <yourilwill.n.h@gmail.com>
2020-01-18 19:42:00 +0900

    finish 13.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,16 +52,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    # beforeアクション
-
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+    # beforeフィルター
 
     # 正しいユーザーかどうか確認
     def correct_user

8eda13b3fa6f2290ef8d2c43a918557dd9bf3f48
yourilwill <yourilwill.n.h@gmail.com>
2020-01-26 17:46:24 +0900

    finish 14.2.5

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require_relative 'boot'
 
 require 'rails/all'
@@ -10,6 +11,9 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
